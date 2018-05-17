9d9a96bea466a36c0b579f6c5520d1fef3f52f7a
Efren <ejimenezdelgado@gmail.com>
2018-05-16 19:07:17 -0600

    clase 14

--- Semana 14/acceso_datos/cliente_acceso_datos.py
+++ Semana 14/acceso_datos/cliente_acceso_datos.py
@@ -1,3 +1,18 @@
 #Fecha: 16/05/2018
 #Objetivo: Realizar un sistema para el manejo de clientes
 
+from constantes import path
+
+class ClienteAccesoDatos:
+
+    def __init__(self):
+        return
+
+    #Crear un cliente
+    def CrearCliente(self,cliente):
+        archivo = open(path + "\archivos\cliente.txt", "a+")
+        archivo.write(str(self.cedula) + "," +
+                      str(self.nombre) + "," +
+                      str(self.sexo) + "," +
+                      str(self.direccion) +"\n")
+        archivo.close()
