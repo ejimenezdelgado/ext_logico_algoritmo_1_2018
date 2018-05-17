0414f7c2a97cdd465f98bac6555f92d3560c3a28
Efren <ejimenezdelgado@gmail.com>
2018-05-16 20:08:21 -0600

    clase 14

--- Semana 14/acceso_datos/cliente_acceso_datos.py
+++ Semana 14/acceso_datos/cliente_acceso_datos.py
@@ -2,6 +2,7 @@
 #Objetivo: Realizar un sistema para el manejo de clientes
 
 from constantes import path
+from entidades import Cliente
 
 class ClienteAccesoDatos:
 
@@ -11,8 +12,74 @@ class ClienteAccesoDatos:
     #Crear un cliente
     def CrearCliente(self,cliente):
         archivo = open(path + "\archivos\cliente.txt", "a+")
-        archivo.write(str(self.cedula) + "," +
-                      str(self.nombre) + "," +
-                      str(self.sexo) + "," +
-                      str(self.direccion) +"\n")
+        archivo.write(str(cliente.cedula) + "," +
+                      str(cliente.nombre) + "," +
+                      str(cliente.sexo) + "," +
+                      str(cliente.direccion) +"\n")
         archivo.close()
+
+    # Elimina un cliente
+    def EliminarCliente(self, cliente):
+        archivo = open(path + "\archivos\cliente.txt", "r")
+        escribir = ""
+        for linea in archivo.readlines():
+            if linea.split(',')[0] != str(cliente.cedula):
+                escribir += linea
+        archivo.close()
+        archivo = open(path + "\archivos\cliente.txt" "w")
+        archivo.write(escribir)
+        archivo.close()
+
+    # Edita un cliente
+    def EditarCliente(self, cliente):
+        archivo = open(path + "\archivos\cliente.txt", "r")
+        escribir = ""
+        for linea in archivo.readlines():
+            if linea.split(',')[0] == str(cliente.cedula):
+                escribir += \
+                    (str(cliente.cedula) + "," +
+                      str(cliente.nombre) + "," +
+                      str(cliente.sexo) + "," +
+                      str(cliente.direccion) +"\n")
+            else:
+                escribir += linea
+        archivo.close()
+        archivo = open(path + "\archivos\cliente.txt", "w")
+        archivo.write(escribir);
+        archivo.close()
+
+    # Obtiene un cliente
+    def ObtenerCliente(self, cliente):
+        archivo = open(path + "\archivos\cliente.txt", "r")
+        for linea in archivo.readlines():
+            if linea.split(',')[0] == str(cliente.cedula):
+                cliente = Cliente()
+                cliente.cedula=linea.split(',')[0]
+                cliente.nombre=linea.split(',')[1]
+                cliente.sexo=linea.split(',')[2]
+                cliente.direccion=linea.split(',')[3]
+            return cliente
+
+    # Obtiene los clientes
+    def ObtenerClientes(self):
+        archivo = open(path + "\archivos\cliente.txt", "r")
+        lista=[]
+        for linea in archivo.readlines():
+            cliente = Cliente()
+            cliente.cedula = linea.split(',')[0]
+            cliente.nombre = linea.split(',')[1]
+            cliente.sexo = linea.split(',')[2]
+            cliente.direccion = linea.split(',')[3]
+            lista.append(cliente)
+        return lista
+
+    # Obtiene la cantidad de clientes ingresados
+    def CantidadClientes(self):
+        contador = 0
+        try:
+            archivo = open(path + "\archivos\cliente.txt", "r+")
+            contador = len(archivo.readlines())
+            archivo.close()
+        except ValueError:
+            print(ValueError)
+        return contador
---
+++ Semana 14/logica/cliente_logica.py
@@ -0,0 +1,34 @@
+#Fecha: 16/05/2018
+
+from acceso_datos import ClienteAccesoDatos
+
+class cliente_logica:
+    def CrearCliente(self, cliente):
+        clienteAccesoDatos= ClienteAccesoDatos()
+        clienteAccesoDatos.CrearCliente(cliente)
+
+    # Elimina un cliente
+    def EliminarCliente(self, cliente):
+        clienteAccesoDatos = ClienteAccesoDatos()
+        clienteAccesoDatos.EliminarCliente(cliente)
+
+    # Edita un cliente
+    def EditarCliente(self, cliente):
+        clienteAccesoDatos = ClienteAccesoDatos()
+        clienteAccesoDatos.EditarCliente(cliente)
+
+    # Obtiene un cliente
+    def ObtenerCliente(self, cliente):
+        clienteAccesoDatos = ClienteAccesoDatos()
+        return clienteAccesoDatos.ObtenerCliente(cliente)
+
+
+    # Obtiene los clientes
+    def ObtenerClientes(self):
+        clienteAccesoDatos = ClienteAccesoDatos()
+        return clienteAccesoDatos.ObtenerClientes()
+
+    # Obtiene la cantidad de clientes ingresados
+    def CantidadClientes(self):
+        clienteAccesoDatos = ClienteAccesoDatos()
+        return clienteAccesoDatos.CantidadClientes()
